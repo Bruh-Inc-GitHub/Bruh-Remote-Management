71104d36c360b9ccd4da4184c58b132b51a32384
Eric Reed <mrericcoolnessreed@gmail.com>
2020-01-13 09:18:26 -0500

    Added Xcode project

---
+++ Bruh Remote Management/Bruh Remote Management/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Bruh Remote Management
+//
+//  Created by Eric Reed on 1/13/20.
+//  Copyright © 2020 Bruh, Inc. All rights reserved.
+//
+
+import Foundation
+
+print("bruh")
+
